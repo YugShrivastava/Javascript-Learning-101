7bed3ee73d77c83e0c67da5a2e171961a6140326
YugShrivastava <shrivastavayug04@gmail.com>
2024-06-27 01:03:52 +0530

    Trying to add display to the game, whackIt function, display function, popUp function, Have ti try after the exams. Will be planning and coding after some time. Idea: a simple whack a mole game which uses random time, random number, single mole, a selector for difficulty that tampers with time, a selector for making the 3 x 3 grid bigger, and some more good thingamajigs.

--- Whack-A-Mole/index.js
+++ Whack-A-Mole/index.js
@@ -1,6 +1,8 @@
 const whackCircles = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const moles = document.querySelectorAll(".circle");
 let place = Math.floor(Math.random() * 9 + 1);
-let round = 0;
+let round = 0,
+  score = 0;
 
 function getRandomPlace(placed) {
   place = Math.floor(Math.random() * 9 + 1);
@@ -9,7 +11,13 @@ function getRandomPlace(placed) {
   return place;
 }
 
-function displayMole(place){
+function whackIt(event) {
+  event.target.innerHTML = "";
+  score++;
+  playRound();
+}
+
+function displayMole(place) {
   const mole = document.getElementById(place);
   mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
 }
@@ -36,7 +44,14 @@ function startGame() {
     playRound(place);
   }
 
+  console.log("Score = ", score);
   console.log("\n\nCompleted.");
 }
 
+moles.forEach((mole) => {
+  mole.addEventListener("click", whackIt(event));
+});
+
 // startGame();
+
+// AA7339 F4F3F3 FCFCFC
