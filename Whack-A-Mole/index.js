a9dc6186740c88675c760c9f17fae5f5010a95a3
YugShrivastava <shrivastavayug04@gmail.com>
2024-06-27 17:52:45 +0530

    Have to start new, for getting into time.

--- Whack-A-Mole/index.js
+++ Whack-A-Mole/index.js
@@ -1,6 +1,7 @@
 const moles = document.querySelectorAll(".circle");
 let place = Math.floor(Math.random() * 9 + 1);
-let score = 0, round = 0;
+let score = 0,
+    round = 0;
 
 // let time = Math.floor(Math.random() * 1000);
 // console.log(time);
@@ -10,54 +11,54 @@ let score = 0, round = 0;
 // }
 
 function getRandomPlace(placed) {
-  place = Math.floor(Math.random() * 9 + 1);
+    place = Math.floor(Math.random() * 9 + 1);
 
-  if (placed === place) place = getRandomPlace(place);
-  return place;
+    if (placed === place) place = getRandomPlace(place);
+    return place;
 }
 
 function displayMole(place) {
-  const mole = document.getElementById(place);
-  mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
+    const mole = document.getElementById(place);
+    mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
 }
 displayMole(place);
 
-function scoreDisplay(){
-  const scoreDiv = document.querySelector(".scoreDiv");
-  scoreDiv.textContent = `Score = ${++score}`;
+function scoreDisplay() {
+    const scoreDiv = document.querySelector(".scoreDiv");
+    scoreDiv.textContent = `Score = ${++score}`;
 }
 
-function dialogOpen(round){
-  const dialog = document.querySelector("dialog");
-  dialog.showModal();
+function dialogOpen(round) {
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
 
-  const replay = document.querySelector("#replay");
+    const replay = document.querySelector("#replay");
 
-  replay.addEventListener("click", () => {
+    replay.addEventListener("click", () => {
+        score = -1;
+        scoreDisplay();
 
-    score = -1;
-    scoreDisplay();
-  
-    const roundDiv = document.querySelector(".roundDiv");
-    roundDiv.textContent = `Round = ${round}`;
+        const roundDiv = document.querySelector(".roundDiv");
+        roundDiv.textContent = `Round = ${round}`;
 
-    dialog.close();
-  });
+        dialog.close();
+    });
 }
 
 moles.forEach((mole) => {
-  mole.addEventListener("click", () => {
-    if (score < 10){
-      if (mole.getAttribute("id") == place) {
-        mole.innerHTML = "";
-        scoreDisplay();
-        place = getRandomPlace(place);
-        displayMole(place);
-      } else {}
-    }
-    if (score === 10) {
-      dialogOpen(++round);
-      return;
-    }
-  });
+    mole.addEventListener("click", () => {
+        if (score < 10) {
+            if (mole.getAttribute("id") == place) {
+                mole.innerHTML = "";
+                scoreDisplay();
+                place = getRandomPlace(place);
+                displayMole(place);
+            } else {
+            }
+        }
+        if (score === 10) {
+            dialogOpen(++round);
+            return;
+        }
+    });
 });
