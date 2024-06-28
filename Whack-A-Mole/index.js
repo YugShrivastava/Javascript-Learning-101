b3143a32b08a311b0fe2be20b7f542c2636bccab
YugShrivastava <shrivastavayug04@gmail.com>
2024-06-28 23:48:08 +0530

    added functionality of timed moles, have to fix the bug of mole getting whacked.

--- Whack-A-Mole/index.js
+++ Whack-A-Mole/index.js
@@ -1,14 +1,15 @@
 const moles = document.querySelectorAll(".circle");
 let place = Math.floor(Math.random() * 9 + 1);
 let score = 0,
-    round = 0;
-
+    round = 0,
+    index = -2;
+let timeout;
 // let time = Math.floor(Math.random() * 1000);
 // console.log(time);
 
-// function getRandomTime(min, max){
-//   return time = Math.floor(Math.random() * (max - min) + min);
-// }
+function getRandomTime(min, max){
+  return time = Math.floor(Math.random() * (max - min) + min);
+}
 
 function getRandomPlace(placed) {
     place = Math.floor(Math.random() * 9 + 1);
@@ -18,6 +19,26 @@ function getRandomPlace(placed) {
 }
 
 function displayMole(place) {
+    timeout = setTimeout(() => {
+        if(!timeoutFlag) {
+            console.log("Inside setTimeout");
+    
+            index++;
+            const indexDiv = document.querySelector(".indexDiv");
+            indexDiv.textContent = `Index = ${++index}`;
+            console.log(place);
+            
+            moles.forEach((mole) => {
+                if(mole.getAttribute("id") == place) mole.innerHTML = "";
+            })
+    
+            place = getRandomPlace(place);
+            displayMole(place);
+        }
+        else{
+    
+        }
+    }, getRandomTime(600, 800))
     const mole = document.getElementById(place);
     mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
 }
@@ -26,6 +47,8 @@ displayMole(place);
 function scoreDisplay() {
     const scoreDiv = document.querySelector(".scoreDiv");
     scoreDiv.textContent = `Score = ${++score}`;
+    const indexDiv = document.querySelector(".indexDiv");
+    indexDiv.textContent = `Index = ${++index}`;
 }
 
 function dialogOpen(round) {
@@ -36,6 +59,7 @@ function dialogOpen(round) {
 
     replay.addEventListener("click", () => {
         score = -1;
+        index = -1;
         scoreDisplay();
 
         const roundDiv = document.querySelector(".roundDiv");
@@ -45,20 +69,27 @@ function dialogOpen(round) {
     });
 }
 
+let timeoutFlag = false;
+
+
+
 moles.forEach((mole) => {
     mole.addEventListener("click", () => {
-        if (score < 10) {
+        timeoutFlag = true;
+        if (index < 20) {
             if (mole.getAttribute("id") == place) {
                 mole.innerHTML = "";
                 scoreDisplay();
                 place = getRandomPlace(place);
+
                 displayMole(place);
             } else {
             }
         }
-        if (score === 10) {
+        if (index === 20) {
             dialogOpen(++round);
             return;
         }
+        timeoutFlag = false;
     });
 });
