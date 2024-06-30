0f0b7bfeacb6099a0a2f99153d4cad3a705e54b4
YugShrivastava <shrivastavayug04@gmail.com>
2024-06-30 10:43:37 +0530

    starting new...

--- Whack-A-Mole/script.js
+++ Whack-A-Mole/script.js
@@ -1,65 +1,77 @@
 // Strating New.
 
-const moles = document.querySelectorAll("circle");
-let timeoutID, index = 0, score = 0, round = 0;
+const moles = document.querySelectorAll(".circle");
+let timeoutID,
+    index = 0,
+    score = 0,
+    round = 0;
 
-function getRandomPlace(placed) {
-    let place = Math.floor(Math.random() * 9 + 1);
-
-    if (placed == place) place = getRandomPlace(place);
-    console.log(place);
-    return place;
+function getRandomPlace() {
+    return Math.floor(Math.random() * 9 + 1);
 }
 
 function getRandomTime(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function incrementIndex(){
+function checkForGameOver() {}
+
+function incrementIndex() {
     index++;
     const indexDiv = document.querySelector(".indexDiv");
     indexDiv.innerHTML = `Index = <span>${index}<span>`;
 }
 
-function incrementScore(){
+function incrementScore() {
     score++;
     const scoreDiv = document.querySelector(".scoreDiv");
     scoreDiv.innerHTML = `Score = ${score}`;
 }
 
-function incrementRound(){
+function incrementRound() {
     round++;
     const roundDiv = document.querySelector(".roundDiv");
     roundDiv.innerHTML = `Round = ${round}`;
 }
 
-function timeout(){
-        incrementIndex();
-        if(checkForGameOver()) return;
-}
-
-
-
-function displayMole(min, max, place) {
+function clearDisplayMole(place) {
     moles.forEach((mole) => {
         if (mole.getAttribute("id") == place) {
-            mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
-            timeoutID = setTimeout(timeout(), getRandomTime(min, max));
-
-            return;
+            mole.innerHTML = "";
         }
     });
 }
 
-function clearDisplayMole(place) {}
+function timeout(place) {
+    clearDisplayMole(place);
+    incrementIndex();    
+}
+
+
+
+function displayMole(place) {
+    console.log(place);
+    moles.forEach((mole) => {
+        if (mole.getAttribute('id') == place) {
+            mole.innerHTML = `<img src='./images/mole.svg' alt='mole' id='mole-img'>`;
+        }
+    });
+    return;
+}
 
 function resetAll() {}
 
 function startGame() {
     let gameRounds = 20;
 
-    for (let i = 0; i < gameRounds; i++) {}
+    for (let i = 0; i < gameRounds; i++) {
+        place = getRandomPlace();
+        displayMole(place);
+        console.log(place);
+        timeoutID = setTimeout(timeout(place), getRandomTime(800, 1000));
+        checkForGameOver();
+    }
 }
 
-displayMole(800, 1000, Math.floor(Math.random() * 9 + 1));
 
+startGame();
